6026c8a2dc49c29a03ec7d48de9d08ab0bb6426b
suittizihou <suittizihou@gmail.com>
2020-10-16 16:11:12 +0900

    山本さんのステージを追加

---
+++ StageEditData/MapChip.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="MapChip" tilewidth="32" tileheight="32" tilecount="4" columns="4">
+ <image source="../Sokoban/Resources/Image/MapChip.png" width="128" height="32"/>
+</tileset>
